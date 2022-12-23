be03efaaf0df2bbfe1f32e312a7f7a074a9e5ddf
Guuhp <lg645471@gmail.com>
2022-12-23 04:28:41 -0400

    project completed

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask,jsonify,request,send_file
+import random
+import os
+import json
+import shutil
+from PIL import Image
+import io
+
+app = Flask(__name__)
+PATH="/home/guuhp/Documents/programacao/project_chat-1/static/upload"
+
+@app.route("/new_conversation",methods=["GET"])
+def new_conversation():
+    room_key = random.randint(10,100)
+    os.mkdir(f"{PATH}/{room_key}")
+    path_folder = f'{PATH}/{room_key}'
+    print(path_folder)
+    return jsonify(path=room_key)
+
+@app.route("/join_room",methods=["GET","POST"])
+def join_room():
+    if request.method =="POST":
+        cod_room = request.json["data"]
+        print(cod_room)
+        NEWPATH = f'{PATH}/{cod_room}'
+        if os.path.exists(NEWPATH):
+            message = {"message":True}
+            return jsonify(message)
+        message = {"message":False}
+        return jsonify(message)
+    
+@app.route("/new_message",methods=["POST"])
+def new_message():
+    path_picture = request.files['file']
+    code_room = request.form.get('code_room')
+    user = request.form.get('user')
+    print(path_picture)
+    print(code_room)
+    print(path_picture)
+    if user =="user1":
+        picture = Image.open(io.BytesIO(path_picture.stream.read()))
+        path = f"{PATH}/{code_room}/{user}.png"
+        print(path)
+        picture.save(path)
+    if user =="user2":
+        picture = Image.open(io.BytesIO(path_picture.stream.read()))
+        path = f"{PATH}/{code_room}/{user}.png"
+        picture.save(path)
+
+    return
+
+@app.route("/get_message",methods=["POST"])
+def get_message():
+    cod_room=request.json['cd_room']
+    user=request.json['user']
+    user=str(user)
+    print(cod_room)
+    print(user)
+    if user =="user1":
+        path=f"{PATH}/{cod_room}/user2.png"
+        return send_file(path,'image/png')
+    
+    if user =="user2":
+        path=f"{PATH}/{cod_room}/user1.png"
+        print(path)  
+        return send_file(path,'image/png')
+    
+    
+
+if __name__=='__main__':
+    app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,117 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager,Screen
+from kivy.uix.image import AsyncImage
+from kivy.app import App
+from kivy.lang import Builder
+import requests
+from PIL import Image
+import io
+
+
+IP="http://192.168.101.11:5000"
+USER1=''
+USER2=''
+CDROOM1=''
+CDROOM2=''
+
+class JanelaGerenciador(ScreenManager):
+  pass
+
+class Ip(Screen):
+  pass
+
+class Home(Screen):
+  pass
+
+class Chat(Screen):
+  pass
+
+class CameraClick(Screen):
+  pass
+
+class MeuAplicativo(App):
+  def build(self):
+    self.file = Builder.load_file("main.kv")
+    return self.file
+  
+  def ip(self):
+    global IP
+    IP = self.file.get_screen('ip').ids['inp_ip'].text
+    print(IP)
+    return    
+  
+  def new_conversation(self):
+    global USER1,CDROOM1
+    request=requests.get("http://"+IP+"/new_conversation")
+    json = request.json()
+    room_key = json['path']
+    USER1="user1"
+    CDROOM1 = room_key
+    print(CDROOM1)
+    self.file.get_screen('home').ids['idsala'].text=f'{room_key}'
+    return  
+  
+  def join_room(self):
+    global USER2,CDROOM2
+    cd_room=int(self.file.get_screen('home').ids["inpt_codigo_room"].text)
+    cod_room = {"data":cd_room}
+    print(cod_room)
+    response=requests.post("http://"+IP+"/join_room",json=cod_room)
+    message = response.json()
+    if message['message'] ==True:
+      print("resposta")
+      cod_room =int(self.file.get_screen('home').ids["inpt_codigo_room"].text)
+      USER2="user2"
+      CDROOM2=cd_room
+      print(CDROOM2)
+    if message['message'] ==False:
+      self.file.get_screen('home').ids['idsala'].text=f'codigo da sala inexistente'
+
+  def capture(self):
+    global USER1,USER2,CDROOM1,CDROOM2    
+    if USER1 == "user1":
+        camera = self.file.get_screen('camera').ids['camera']
+        camera.export_to_png("/sdcard/user1.png")
+        data = {"code_room":CDROOM1,"user":"user1"}
+        print(data)
+        file = {'file':open('/sdcard/user1.png','rb')}
+        requests.post("http://"+IP+"/new_message",files=file,data=data)
+
+    if USER2 =="user2":
+        camera = self.file.get_screen('camera').ids['camera']
+        camera.export_to_png("/sdcard/user2.png")
+        data = {"code_room":CDROOM2,"user":"user2"}
+        file = {'file':open('/sdcard/user2.png','rb')}
+        requests.post("http://"+IP+"/new_message",files=file,data=data)
+  def get_message(self):
+    global USER1,USER2,CDROOM1,CDROOM2
+    if USER1 =="user1":
+        cod_room = {"cd_room":CDROOM1,"user":"user1"}
+        request=requests.post("http://"+IP+"/get_message",json=cod_room)
+        print(request.content)
+        image = Image.open(io.BytesIO(request.content))
+        image.save(f'/sdcard/res2.png')
+        image='/sdcard/res2.png'
+        self.file.get_screen('home').ids['imguser'].reload() 
+        self.file.get_screen('home').ids['imguser'].source = image   
+        
+    if USER2 =="user2":
+        cod_room = {"cd_room":CDROOM2,"user":"user2"}
+        print(cod_room)
+        request=requests.post("http://"+IP+"/get_message",json=cod_room)
+        print(request.content)
+        image = Image.open(io.BytesIO(request.content))
+        image.save(f'/sdcard/res1.png')
+        image='/sdcard/res1.png'
+        self.file.get_screen('home').ids['imguser'].reload() 
+        self.file.get_screen('home').ids['imguser'].source = image  
+    
+    
+
+    ''
+
+
+  
+
+MeuAplicativo().run()
