2ea258c06686781ad2616939508dd88ad0fef6c2
Guuhp <lg645471@gmail.com>
2023-01-09 22:59:01 -0400

    code review

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ import io
 IP="http://192.168.101.11:5000"
 USER1=''
 USER2=''
-CDROOM1=''
-CDROOM2=''
-
+CODE_ROOM_1=''
+CODE_ROOM_2=''
+# 
 class JanelaGerenciador(ScreenManager):
   pass
 
@@ -38,72 +38,64 @@ class MeuAplicativo(App):
   def ip(self):
     global IP
     IP = self.file.get_screen('ip').ids['inp_ip'].text
-    print(IP)
     return    
   
   def new_conversation(self):
-    global USER1,CDROOM1
+    global USER1,CODE_ROOM_1
     request=requests.get("http://"+IP+"/new_conversation")
     json = request.json()
     room_key = json['path']
     USER1="user1"
-    CDROOM1 = room_key
-    print(CDROOM1)
+    CODE_ROOM_1 = room_key
     self.file.get_screen('home').ids['idsala'].text=f'{room_key}'
     return  
   
   def join_room(self):
-    global USER2,CDROOM2
-    cd_room=int(self.file.get_screen('home').ids["inpt_codigo_room"].text)
-    cod_room = {"data":cd_room}
-    print(cod_room)
+    global USER2,CODE_ROOM_2
+    code_room=int(self.file.get_screen('home').ids["inpt_codigo_room"].text)
+    cod_room = {"data":code_room}
     response=requests.post("http://"+IP+"/join_room",json=cod_room)
     message = response.json()
     if message['message'] ==True:
-      print("resposta")
       cod_room =int(self.file.get_screen('home').ids["inpt_codigo_room"].text)
       USER2="user2"
-      CDROOM2=cd_room
-      print(CDROOM2)
+      CODE_ROOM_2=code_room
     if message['message'] ==False:
       self.file.get_screen('home').ids['idsala'].text=f'codigo da sala inexistente'
 
+  def verify_user_capture(self,user,code_room):
+    camera = self.file.get_screen('camera').ids['camera']
+    name_photo = f'{user}.png'
+    camera.export_to_png(name_photo)
+    data = {"code_room":code_room,"user":user}
+    file = {'file':open(name_photo,'rb')}
+    requests.post("http://"+IP+"/new_message",files=file,data=data)
+
   def capture(self):
-    global USER1,USER2,CDROOM1,CDROOM2    
+    global USER1,USER2,CODE_ROOM_1,CODE_ROOM_2    
     if USER1 == "user1":
-        camera = self.file.get_screen('camera').ids['camera']
-        camera.export_to_png("/sdcard/user1.png")
-        data = {"code_room":CDROOM1,"user":"user1"}
-        print(data)
-        file = {'file':open('/sdcard/user1.png','rb')}
-        requests.post("http://"+IP+"/new_message",files=file,data=data)
+      self.verify_user_capture("user1",CODE_ROOM_1)
 
     if USER2 =="user2":
-        camera = self.file.get_screen('camera').ids['camera']
-        camera.export_to_png("/sdcard/user2.png")
-        data = {"code_room":CDROOM2,"user":"user2"}
-        file = {'file':open('/sdcard/user2.png','rb')}
-        requests.post("http://"+IP+"/new_message",files=file,data=data)
+      self.verify_user_capture("user2",CODE_ROOM_2)
+      
   def get_message(self):
-    global USER1,USER2,CDROOM1,CDROOM2
+    global USER1,USER2,CODE_ROOM_1,CODE_ROOM_2
     if USER1 =="user1":
-        cod_room = {"cd_room":CDROOM1,"user":"user1"}
+        cod_room = {"code_room":CODE_ROOM_1,"user":"user1"}
         request=requests.post("http://"+IP+"/get_message",json=cod_room)
-        print(request.content)
         image = Image.open(io.BytesIO(request.content))
-        image.save(f'/sdcard/res2.png')
-        image='/sdcard/res2.png'
+        image.save(f'res2.png')
+        image='res2.png'
         self.file.get_screen('home').ids['imguser'].reload() 
         self.file.get_screen('home').ids['imguser'].source = image   
         
     if USER2 =="user2":
-        cod_room = {"cd_room":CDROOM2,"user":"user2"}
-        print(cod_room)
+        cod_room = {"code_room":CODE_ROOM_2,"user":"user2"}
         request=requests.post("http://"+IP+"/get_message",json=cod_room)
-        print(request.content)
         image = Image.open(io.BytesIO(request.content))
-        image.save(f'/sdcard/res1.png')
-        image='/sdcard/res1.png'
+        image.save(f'res1.png')
+        image='res1.png'
         self.file.get_screen('home').ids['imguser'].reload() 
         self.file.get_screen('home').ids['imguser'].source = image  
     
